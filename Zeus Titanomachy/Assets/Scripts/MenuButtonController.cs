877d6c8dbd1542b65f12c36b09ccbdeaf1d3ad27
Wei <K1209861@students.katyisd.org>
2024-03-21 11:01:25 -0500

    UI made for main menu
    coded functions to quit and new game buttons. continue will be left alone for now

---
+++ Zeus Titanomachy/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour {
+
+	// Use this for initialization
+	public int index;
+	public bool pres = false;
+	[SerializeField] bool keyDown;
+	[SerializeField] int maxIndex;
+	public AudioSource audioSource;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Return) && index == 0)
+        {
+            SceneManager.LoadScene("Actual_scene");
+        }
+        if (Input.GetKeyDown(KeyCode.Return) && index == 2)
+        {
+            Application.Quit();
+        }
+        if (Input.GetAxis ("Vertical") != 0){
+			if(!keyDown){
+				if (Input.GetAxis ("Vertical") < 0) {
+					if(index < maxIndex){
+						index++;
+					}else{
+						index = 0;
+					}
+				} else if(Input.GetAxis ("Vertical") > 0){
+					if(index > 0){
+						index --; 
+					}else{
+						index = maxIndex;
+					}
+				}
+				keyDown = true;
+			}
+		}else{
+			keyDown = false;
+		}
+        
+
+    }
+	
+
+}
