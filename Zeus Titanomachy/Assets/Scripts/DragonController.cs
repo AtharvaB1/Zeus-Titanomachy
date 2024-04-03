8a7d63b860aa6eab75fdc0af30f75724f4235088
RockyCheng111 <rockytx2013@gmail.com>
2024-04-03 11:22:38 -0500

    DrFlight Changes

---
+++ Zeus Titanomachy/Assets/Scripts/DragonController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragonController : MonoBehaviour
+{
+    public GameObject[] pathArray;
+    public GameObject[] checkpointArray;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
