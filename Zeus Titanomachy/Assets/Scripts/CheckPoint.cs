5053104349fa7cdd898c6f5f28fef15ca979fee5
RockyCheng111 <rockytx2013@gmail.com>
2024-04-07 16:07:26 -0500

    DrFight and Zeus

---
+++ Zeus Titanomachy/Assets/Scripts/CheckPoint.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckPoint : MonoBehaviour
+{
+    public DragonController dragon;         // Reference to the Dragon GameObject
+    public Transform[] pathGroups; // Array of path group Transforms to move
+
+    private int currentCheckpointIndex = 0; // Index of the current checkpoint in relation to pathGroups
+
+    // Dictionary to map path group index to specific falling amount
+    private Dictionary<int, float> pathFallingAmounts = new Dictionary<int, float>()
+    {
+        { 0, 10f },   // First path falls by 10 units
+        { 1, 15f },   // Second path falls by 15 units
+        { 2, 47f },   // Third path falls by 47 units
+        { 3, 60f },   // Fourth path falls by 60 units
+        { 4, 75f },   // Fifth path falls by 75 units
+        { 5, 80f }    // Sixth path falls by 80 units
+    };
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Trigger the dragon's action immediately
+            TriggerDragonAction(currentCheckpointIndex);
+
+            // Increment the checkpoint index to move to the next checkpoint
+            currentCheckpointIndex++;
+
+            // Check if there are more checkpoints to process
+            if (currentCheckpointIndex < pathGroups.Length)
+            {
+                // Start a coroutine to move the path group after a delay
+                StartCoroutine(MovePathGroupDelayed(pathGroups[currentCheckpointIndex], currentCheckpointIndex));
+            }
+        }
+    }
+
+    private IEnumerator MovePathGroupDelayed(Transform pathGroup, int pathIndex)
+    {
+        yield return new WaitForSeconds(5f); // Wait for 5 seconds
+
+        // Check if the pathIndex exists in the pathFallingAmounts dictionary
+        if (pathFallingAmounts.ContainsKey(pathIndex))
+        {
+            float fallingAmount = pathFallingAmounts[pathIndex];
+            // Move the path group down on the Y-axis by the specified falling amount
+            pathGroup.position -= new Vector3(0f, fallingAmount, 0f);
+        }
+        else
+        {
+            Debug.LogWarning("Path falling amount not specified for index: " + pathIndex);
+        }
+    }
+
+    private void TriggerDragonAction(int checkpointIndex)
+    {
+        // Trigger different actions for the dragon based on the checkpoint index
+        switch (checkpointIndex)
+        {
+            case 0:
+                Debug.Log("Dragon action for checkpoint 1");
+                // Perform specific action for checkpoint 1
+                break;
+            case 1:
+                Debug.Log("Dragon action for checkpoint 2");
+                
+                break;
+            case 2:
+                Debug.Log("Dragon action for checkpoint 3");
+                
+                break;
+            case 3:
+                Debug.Log("Dragon action for checkpoint 4");
+                
+                break;
+            case 4:
+                Debug.Log("Dragon action for checkpoint 5");
+                
+                break;
+            case 5:
+                Debug.Log("Dragon action for checkpoint 5");
+                
+                break;
+        }
+    }
+}
--- Zeus Titanomachy/Assets/Scripts/DragonController.cs
+++ Zeus Titanomachy/Assets/Scripts/DragonController.cs
@@ -6,14 +6,63 @@ public class DragonController : MonoBehaviour
 {
     public GameObject[] pathArray;
     public GameObject[] checkpointArray;
+    private Animator anim;
+    int IdleSimple;
+    int IdleAgressive;
+    int IdleRestless;
+    int Walk;
+    int BattleStance;
+    int Bite;
+    int Drakaris;
+    int FlyingFWD;
+    int FlyingAttack;
+    int Hover;
+    int Lands;
+    int TakeOff;
+    int Die;
     void Start()
     {
-        
+        anim = GetComponent<Animator>();
+        IdleSimple = Animator.StringToHash("IdleSimple");
+        IdleAgressive = Animator.StringToHash("IdleAgressive");
+        IdleRestless = Animator.StringToHash("IdleRestless");
+        Walk = Animator.StringToHash("Walk");
+        BattleStance = Animator.StringToHash("BattleStance");
+        Bite = Animator.StringToHash("Bite");
+        Drakaris = Animator.StringToHash("Drakaris");
+        FlyingFWD = Animator.StringToHash("FlyingFWD");
+        FlyingAttack = Animator.StringToHash("FlyingAttack");
+        Hover = Animator.StringToHash("Hover");
+        Lands = Animator.StringToHash("Lands");
+        TakeOff = Animator.StringToHash("TakeOff");
+        Die = Animator.StringToHash("Die");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (anim.GetCurrentAnimatorStateInfo(0).IsName("IdleSimple"))
+            {
+                anim.SetBool(IdleSimple, false);
+                anim.SetBool(IdleAgressive, false);
+                anim.SetBool(IdleRestless, false);
+                anim.SetBool(Walk, true);
+                anim.SetBool(BattleStance, false);
+                anim.SetBool(Bite, false);
+                anim.SetBool(Drakaris, false);
+                anim.SetBool(FlyingFWD, false);
+                anim.SetBool(FlyingAttack, false);
+                anim.SetBool(Hover, false);
+                anim.SetBool(Lands, false);
+                anim.SetBool(TakeOff, false);
+                anim.SetBool(Die, false);
+
+            }
+        }
+
     }
 }
+
