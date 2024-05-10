f6250ec55b8175e7677976feb742dbbf2b307cbc
AtharvaB1 <k1910791@students.katyisd.org>
2024-05-10 11:15:24 -0500

    music
    music

--- Zeus Titanomachy/Assets/Scripts/Troll_SCript.cs
+++ Zeus Titanomachy/Assets/Scripts/Troll_SCript.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class Troll_SCript : MonoBehaviour
@@ -10,7 +12,7 @@ public class Troll_SCript : MonoBehaviour
     MeshCollider mace;
     public float speed = 10f;
     public GameObject MC;
-    
+    public float raydist = 4f;
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,16 @@ public class Troll_SCript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-            this.gameObject.transform.LookAt(MC.transform.position);
+        this.gameObject.transform.position = transform.forward * speed;
+        RaycastHit hit;
+        Debug.DrawRay(this.gameObject.transform.position, this.gameObject.transform.TransformDirection(Vector3.forward),Color.blue,20000,false);
+        //this.gameObject.transform.rotation.x = Quaternion.identity;
+        if (Physics.Raycast(this.gameObject.transform.position, this.gameObject.transform.TransformDirection(Vector3.forward), out hit, raydist, 0, QueryTriggerInteraction.Ignore))
+        {
+            Debug.Log("tre");
+
+        }
+        else
+            Debug.Log("fals");
     }
 }
